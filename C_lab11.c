5bbeb3ae945c5707f974359f1fd6b2e42a625347
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-04-01 10:46:10 -0700

    Update C_lab11.c

--- C_lab11.c
+++ C_lab11.c
@@ -1,22 +1,20 @@
+
+
 #include<stdio.h>
 int main()
 {
-  int num, i, count=0;
-  printf("enter the number");
-  scanf("%d", &num);
-  i=2;
-  while(i<=num/2)
-   {
-   if(num%i==0)
-    {
-      count = 1;
-      break;
-    }
-    i++;
+ int num, i, temp = 0;
+ int sum = 0;
+ printf("enter the number");
+ scanf("%d", &num);
+ while(num>0)
+  {
+    temp = num%10;
+    sum = sum + temp;
+    num= num/10;
   }
-  if(count==0)
-   printf("it is a prime number");
-  else
-    printf(" it is not a prime number");
+ printf("\n sum = %d", sum);
+
+
  return 0;
 }
