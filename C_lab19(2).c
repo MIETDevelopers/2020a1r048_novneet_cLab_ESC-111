485c40245d38fe6ee66ab8fc8da8edf7887d74f4
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-04-05 10:22:56 -0700

    string operation copy

---
+++ C_lab19(2).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char a[20], b[20];
+
+   printf(" enter the name\n");
+   scanf("%s", a);
+    strcpy(b,a);
+   printf("%s",b );
+   return 0;
+
+
+}
