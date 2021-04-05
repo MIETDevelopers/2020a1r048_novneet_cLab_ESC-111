0ebd3092578d1ccc45c7b12468dc79f56b81e256
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-04-05 10:27:58 -0700

    simple calculator by using switch case & function

---
+++ C_Lab20.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+ int main()
+ {
+  char a[20];
+  int length,k;
+  printf("enter the name: \n");
+  scanf("%s", a);
+  length = strlen(a);
+  length = length - 1;
+  k=0;
+  while( length > k)
+  {
+    if(a[length]!= a[k])
+    {
+      printf("%s is not palindrome", a );
+      break;
+
+    }
+     length--;
+     k++;
+
+  printf("%s is the palindrome", a);
+ }
+  return 0;
+}
