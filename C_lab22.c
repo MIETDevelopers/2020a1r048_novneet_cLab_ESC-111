a897b16d0d737f2aafd9f4c2cd5ecdcbde55f227
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-04-05 10:30:56 -0700

    fibonacci series using recursion

---
+++ C_lab22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact(int n)
+{
+  if(n==0)
+  return 1;
+  else
+  return n*fact(n-1);
+
+}
+ int main()
+ {
+  int result, n;
+  printf("enter the value");
+  scanf("%d", &n);
+  result = fact(n);
+  printf("%d", result);
+
+  return 0;
+}
