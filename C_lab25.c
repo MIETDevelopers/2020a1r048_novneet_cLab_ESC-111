5fd65c924b7f3f5b57c4f7450452e5a90320979c
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-04-05 10:54:04 -0700

    call by value.

---
+++ C_lab25.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int swap(int x, int y)
+{
+ int temp = x;
+  x = y;
+  y = temp;
+  printf(" after swap x = %d and y = %d\n", x,y);
+}
+int main()
+{
+  int a = 10, b = 20;
+  printf(" before swap a = %d and b = %d\n", a, b);
+  // call by value
+  swap(a,b);
+  printf("after swap a = %d and b = %d", a,b);
+  return 0;
+}
